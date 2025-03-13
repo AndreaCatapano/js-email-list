10160b37d90837869e79040b11e9cd65589f5971
AndreaCatapano <a.catapano96@gmail.com>
2025-03-13 15:13:37 +0100

    feat: completed array for ten result with cycle for

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const emailList = [];
 const urlAPI = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-axios.get(urlAPI)
-.then( response => { 
-    console.log("api aperta")
-    const email = response.data.response;
-    emailList.push(email)
-})
-.catch(error => console.error(error));
+for (let i = 0; i < 10; i++) {
+    axios.get(urlAPI)
+        .then(response => {
+            emailList[i] = response.data.response;
+        })
+        .catch(error => console.error(error));
+}
 
 console.log(emailList)
