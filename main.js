ecf0040e7fcc17f509101b6b0f20ac241188e2c5
AndreaCatapano <a.catapano96@gmail.com>
2025-03-13 15:09:20 +0100

    feat: added axios and completed first call to API

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Ciao")
+const emailList = [];
+const urlAPI = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+axios.get(urlAPI)
+.then( response => { 
+    console.log("api aperta")
+    const email = response.data.response;
+    emailList.push(email)
+})
+.catch(error => console.error(error));
+
+console.log(emailList)
